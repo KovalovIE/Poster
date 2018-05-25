b6d1c58cbf9c1c444c29570a770c593ad82a39ba
KovalovIE <kovalevie1@gmail.com>
2018-05-25 13:25:19 +0300

    third

--- src/views/script.js
+++ src/views/script.js
@@ -5,39 +5,135 @@ var dishes = [
         "title": "Говядина",
         "id": "1beef",
         "price": "200",
-        "thumbnailUrl": "beef.jpeg"
+        "thumbnailUrl": "beef.jpeg",
+        "dblMenu": [
+             {
+                  "title": "Жареная говядина",
+                  "id": "7beef",
+                  "price": "10",
+                  "thumbnailUrl": "beef01.png"
+              },
+              {
+                  "title": "Вареная говядина",
+                  "id": "8beef",
+                  "price": "20",
+                  "thumbnailUrl": "beef02.jpg"
+              }           
+        ]
     },
     {
         "title": "Курица",
         "id": "2chicken",
         "price": "100",
-        "thumbnailUrl": "chicken.jpg"
+        "thumbnailUrl": "chicken.jpg",
+        "dblMenu": [
+             {
+                  "title": "Жареная курица",
+                  "id": "9chicken",
+                  "price": "30",
+                  "thumbnailUrl": "chicken01.jpg"
+              },
+              {
+                  "title": "Вареная курица",
+                  "id": "10chicken",
+                  "price": "140",
+                  "thumbnailUrl": "chicken02.jpg"
+              }           
+        ]
     },
     {
         "title": "Свинина",
         "id": "3pork",
         "price": "110",
-        "thumbnailUrl": "pork.jpg"
+        "thumbnailUrl": "pork.jpg",
+        "dblMenu": [
+             {
+                  "title": "Жареная свинина",
+                  "id": "11pork",
+                  "price": "210",
+                  "thumbnailUrl": "pork01.jpg"
+              },
+              {
+                  "title": "Вареная свинина",
+                  "id": "12pork",
+                  "price": "180",
+                  "thumbnailUrl": "pork02.jpg"
+              }           
+        ]
     },
     {
         "title": "Суп",
         "id": "4soup",
         "price": "120",
-        "thumbnailUrl": "soup.jpg"
+        "thumbnailUrl": "soup.jpg",
+        "dblMenu": [
+             {
+                  "title": "Суп 01",
+                  "id": "13soup",
+                  "price": "100",
+                  "thumbnailUrl": "soup01.jpg"
+              },
+              {
+                  "title": "Суп 02",
+                  "id": "14soup",
+                  "price": "150",
+                  "thumbnailUrl": "soup02.jpg"
+              }           
+        ]
     },
     {
         "title": "Сок",
         "id": "5juice",
         "price": "130",
-        "thumbnailUrl": "juice.jpg"
+        "thumbnailUrl": "juice.jpg",
+        "dblMenu": [
+             {
+                  "title": "Апельсиновый сок",
+                  "id": "15juice",
+                  "price": "40",
+                  "thumbnailUrl": "juice01.jpg"
+              },
+              {
+                  "title": "Томатный сок",
+                  "id": "16juice",
+                  "price": "42",
+                  "thumbnailUrl": "juice02.jpg"
+              },
+              {
+                  "title": "Яблочный сок",
+                  "id": "17juice",
+                  "price": "3",
+                  "thumbnailUrl": "juice03.jpg"
+              }           
+        ]
     },
     {
         "title": "Фрукты",
         "id": "6fruits",
         "price": "140",
-        "thumbnailUrl": "fruits.jpg"
+        "thumbnailUrl": "fruits.jpg",
+        "dblMenu": [
+             {
+                  "title": "Клубника",
+                  "id": "18fruits",
+                  "price": "60",
+                  "thumbnailUrl": "fruits01.jpg"
+              },
+              {
+                  "title": "Бананы",
+                  "id": "19fruits",
+                  "price": "35",
+                  "thumbnailUrl": "fruits02.jpg"
+              },
+              {
+                  "title": "Яблоки",
+                  "id": "20fruits",
+                  "price": "35",
+                  "thumbnailUrl": "fruits03.jpg"
+              }           
+        ]
     }
-]
+];
 
 var menuList = document.getElementById('menu-list');
 var aside = document.getElementById('aside');
@@ -54,72 +150,50 @@ dishes.forEach( function(dish) {
   figure.appendChild(img);
   var figcaption = document.createElement('figcaption');
   figcaption.innerHTML = dish.title;
-  figcaption.style.background = "white";
-  figcaption.style.paddingLeft = "10px";
   figure.appendChild(figcaption);
 });
 
- var table = document.createElement('table');
-  aside.appendChild(table);
-  table.style.borderCollapse = 'collapse';
-  table.style.width = '100%';
-  table.setAttribute('id', 'myTable');
-    for(var i = 0; i < 1; i++) {
-          var tr = document.createElement('tr');
-          table.appendChild(tr);
-          //tr.style.marginBottom = "5px";
-        for(var j = 0; j < 4; j++) {
-          var td = document.createElement('td');
-          tr.appendChild(td);
-          td.style.borderBottom = '1px solid black';      
-          //td.style.paddingLeft = '5px';
-          if (i === 0 && j === 0) {
-            td.appendChild(document.createTextNode('Наименование'));
-          } else if (i === 0 && j === 1) {
-            td.appendChild(document.createTextNode('Кол-во'));
-          } else if (i === 0 && j === 2){
-            td.appendChild(document.createTextNode('Цена'));
-          } else if (i === 0 && j === 3){
-            td.appendChild(document.createTextNode('Итого'));
-          }
-        }
+    menuList.addEventListener('click', function(event) {      
+      var goodsPlus = document.querySelector('.main_new-goods');
+      var target = event.target;
+      function findId(goods) { 
+          return goods.id === target.id;
       }
-
-  var tableSum = document.createElement('table');
-    tableTotal.appendChild(tableSum);
-    tableSum.style.borderCollapse = 'collapse';
-    tableSum.style.borderTop = '1px solid grey';
-    tableSum.style.width = '100%';
-    tableSum.style.height = '80px';
-    tableSum.setAttribute('id', 'myTable-sum');
-
-    for(var i = 0; i < 2; i++) {
-          var tr = document.createElement('tr');
-          tableSum.appendChild(tr);
-          //tr.style.marginBottom = "5px";
-        for(var j = 0; j < 3; j++) {
-          var td = document.createElement('td');
-          tr.appendChild(td);
-          td.style.width = 'calc(100%/3)';
-          //td.style.borderBottom = '1px solid black';      
-          //td.style.paddingLeft = '5px';
-          if (i === 0 && j === 0) {
-            td.appendChild(document.createTextNode('Итого'));
-          } else if (i === 0 && j === 1) {
-            td.appendChild(document.createTextNode(''));
-            td.setAttribute('id', 'total-sum');
-          } else if (i === 0 && j === 2){
-            td.appendChild(document.createTextNode('К оплате'));
-          } else if (i === 1 && j === 0){
-            td.appendChild(document.createTextNode('Скидка'));
-          } else if (i === 1 && j === 1){
-            td.appendChild(document.createTextNode('------'));
-          } else if (i === 1 && j === 2){
-            td.appendChild(document.createTextNode(''));
-            td.setAttribute('id', 'total-sum-discount');
-          } 
-        }
+      //console.log(target.id)
+      var goods = dishes.find(findId);
+      //console.log(goods)
+      var elements = [];
+      
+      for (var x in goods) {
+        elements.push(goods[x]);
       }
+      //console.log(elements[4])
+      if (elements[4] == undefined) {
+        return;
+      } else {
+      menuList.innerHTML = '';
+      var div = document.createElement('div');
+      goodsPlus.appendChild(div);
+      div.innerHTML = elements[0];
+      div.classList.add('main_goods');
+      //div.setAttribute('id','aaa')
+
+      elements[4].forEach( function(dish) {
+        var figure = document.createElement('figure');
+        var img = new Image();
+        img.id = dish.id;
+        img.src = dish.thumbnailUrl;
+        img.classList.add('img-size')
+        menuList.appendChild(figure);
+        figure.appendChild(img);
+        var figcaption = document.createElement('figcaption');
+        figcaption.innerHTML = dish.title;
+        figure.appendChild(figcaption);
+      });
+      }
+    })
+
+
 
     menuList.addEventListener('dblclick', function(event) {
       var table1 = document.getElementById("myTable");
@@ -131,24 +205,31 @@ dishes.forEach( function(dish) {
       var cell4 = row.insertCell(3);
       cell4.setAttribute('name','total')
       var target = event.target;
+
       function findId(goods) { 
           return goods.id === target.id;
-      }
-
-      var goods = dishes.find(findId);
+      }      
+      //console.log(target.id)
+      //var goods = {};
       var elements = [];
-
-      for (var x in goods) {
-        elements.push(goods[x]);
-      }
-
+      var result = dishes.map(dish => dish.dblMenu);
+      function returnElem() {
+        for (var i = 0; i < result.length; i++) {
+          //console.log(result[i]);
+          var goods = result[i].find(findId);
+          
+          for (var x in goods) {
+            elements.push(goods[x]);
+          }
+          //console.log(elements[1]);
+          if(elements[1] === target.id) return elements;
+        }
+      };
+      returnElem();
+      
       cell1.innerHTML = elements[0];
-      //cell2.innerHTML = inputNumber;
       cell2.innerHTML = '<input type="number" name="quantity" min="1" max="99" value="1">';
       cell3.innerHTML = elements[2];
-      //cell3.innerHTML = '<input type="number" name="summ" min="1" max="99">';
-      //cell4.innerHTML = itog;
-      //cell4.innerHTML = '<input type="number" name="total" min="0" max="99999999">';
       cell4.innerHTML = elements[2];
 
       $(document).on('change','input[type="number"]',function(){    
@@ -157,36 +238,12 @@ dishes.forEach( function(dish) {
            //var Price= tr.find('td:eq(2)').html();
            var Price= tr.find('.cena').html();              
           var total= (Quantity * Price);
-          console.log(total);
+          //console.log(total);
           tr.find('[name="total"]').html(total);
       });
-
-      // var input = document.getElementById('input1');
-      // input.oninput = function() {
-      //   // input.addEventListener('input', function() {
-      //   //   var result = document.getElementById('result');
-      //   //   result.innerHTML = input.value*price;
-      //   // })
-      //   var result = document.getElementById('result');
-      //   result.innerHTML = input.value * cell3.innerHTML;
-      // };
-
-
-
-// var input = document.getElementsByName('quantity')[0];
-// console.log(input);
-// input.addEventListener('input', function (evt) {
-//     console.log(this.value);
-// });
-// window.addEventListener('input', function (e) {
-//  console.log(this.value);
-// }, false);
-
-      // alert(target.price);
-      //   if(target.id === "1") {
-      // //alert(target.id);
-      //console.log(goods);
 }, false);
+
+
 
     send.addEventListener('click', function() {
       var table1 = document.getElementById("myTable");
@@ -196,7 +253,6 @@ dishes.forEach( function(dish) {
       for (m = 1; m < numberRow; m++) {
         var sumRows = table1.rows[m].cells[3].innerHTML;
         totalSum += +sumRows;
-        
       }
       //console.log(totalSum);
       var totalSumElement = document.getElementById("total-sum");
@@ -205,5 +261,44 @@ dishes.forEach( function(dish) {
       totalSumDiscount.innerHTML = totalSum;
       totalSumDiscount.style.background = "yellow";
       totalSumDiscount.style.fontSize = "25px";
+    })
+
+
+
+    var startGoods = document.getElementById("start-menu");
+    startGoods.addEventListener('click', function() {
+      var mainNewGoods = document.querySelector('.main_new-goods');
+      menuList.innerHTML = '';
+      while (mainNewGoods.lastChild.id !== 'start-menu') {
+          mainNewGoods.removeChild(mainNewGoods.lastChild);
+
+      }
+
+      dishes.forEach( function(dish) {
+        var figure = document.createElement('figure');
+        var img = new Image();
+        img.id = dish.id;
+        img.src = dish.thumbnailUrl;
+        img.classList.add('img-size')
+        menuList.appendChild(figure);
+        figure.appendChild(img);
+        var figcaption = document.createElement('figcaption');
+        figcaption.innerHTML = dish.title;
+        figure.appendChild(figcaption);
+      });
+    })
+
+
+
+    var print = document.getElementById("print-table");
+    print.addEventListener('click', function() {
+      function printDiv() {
+         var asideTable = document.getElementById("aside");
+         var tableSum = document.querySelector('.table-sum')
+         window.frames["print_frame"].document.body.innerHTML = asideTable.innerHTML + '----------------' + tableSum.innerHTML;
+         window.frames["print_frame"].window.focus();
+         window.frames["print_frame"].window.print();
+       };
+      printDiv();
     })
 }
